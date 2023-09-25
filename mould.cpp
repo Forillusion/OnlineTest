1aae97fdfa8631e78a74fc81cce2ef2f070a2ae8
Forillusion <78995328+Forillusion@users.noreply.github.com>
2023-09-25 10:06:39 +0800

    Create mould.cpp

---
+++ mould.cpp
@@ -0,0 +1,18 @@
+/******************************************
+ * This Program is used to solve luogo.com problem P5707 late for school.
+ * 
+ * Filename:P5707.cpp
+ * Created 24 November 2023 by Forillusion
+ * Copyright 2023 by Forillusion
+ * This program is licensed under the GPLv3 license.
+******************************************/
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
